1b7c3a8cae08da24b63c3b00ca0cc347567dc4f1
tranhuy105 <imhuy105@gmail.com>
2024-04-20 01:20:19 +0700

    static-resource-service done

--- src/main/java/com/huy/airbnbserver/image/ImageController.java
+++ src/main/java/com/huy/airbnbserver/image/ImageController.java
@@ -3,15 +3,16 @@ package com.huy.airbnbserver.image;
 import com.huy.airbnbserver.image.converter.ImageToImageDtoConverter;
 import com.huy.airbnbserver.system.Result;
 import com.huy.airbnbserver.system.StatusCode;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.awt.*;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 import java.util.zip.DataFormatException;
 
 @RestController
@@ -23,7 +24,17 @@ public class ImageController {
     private final ImageToImageDtoConverter imageToImageDtoConverter;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public Result uploadImage(@RequestParam("images") List<MultipartFile> files) throws IOException {
+    public Result uploadImage(@NotNull @RequestParam("images") List<MultipartFile> files) throws IOException {
+        boolean validFilePresent = files.stream()
+                .allMatch(file -> {
+                    String contentType = file.getContentType();
+                    return contentType != null && contentType.startsWith("image/");
+                });
+
+        if (!validFilePresent) {
+            return new Result(false, StatusCode.INVALID_ARGUMENT, "Invalid image files were provided", null);
+        }
+
         var imageDtos = imageService.upload(files).stream()
                 .map(imageToImageDtoConverter::convert)
                 .toList();
