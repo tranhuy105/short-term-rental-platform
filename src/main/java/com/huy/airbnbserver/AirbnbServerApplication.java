45b8976878b5a0ee1bd67dbb1749dca617c66aaf
tranhuy105 <imhuy105@gmail.com>
2024-04-19 15:04:35 +0700

    first commit!

---
+++ src/main/java/com/huy/airbnbserver/AirbnbServerApplication.java
@@ -0,0 +1,13 @@
+package com.huy.airbnbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbnbServerApplication.class, args);
+    }
+
+}
